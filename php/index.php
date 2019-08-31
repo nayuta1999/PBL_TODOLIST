4ff5f6e1268405720698c2ce69086ea83758ade8
nayuta1999 <youmu331@gmail.com>
2019-08-31 22:15:02 +0900

    initial commit

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+    $json=[
+        'TODO' => [
+            '環境構築' => true,
+            'jsonの返し方' => false,
+        ]
+    ];
+    //header("Access-Control-Allow-Origin: *");
+    echo json_encode($json);
+?>
