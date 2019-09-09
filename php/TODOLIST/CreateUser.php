ee1fbc7a6af865a890fd8ab552cd3574012bfbe6
nayuta1999 <youmu331@gmail.com>
2019-09-09 14:20:32 +0900

    CreateUserの軽微な修正

--- php/TODOLIST/CreateUser.php
+++ php/TODOLIST/CreateUser.php
@@ -2,5 +2,5 @@
     require_once('DataBase.php');
     $test = new Database();
     $test->User_insert($_POST['user'],$_POST['table'],$_POST['pass']);
-    echo $_POST['User'];
+    echo $_POST['User'].'を作成しました';
 ?>
